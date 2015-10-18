98e9ee0ddbcb69ec44af712b7b0f4770725a7c67
Freehaha <freehaha@gmail.com>
2015-10-19 07:28:16 +1100

    return object

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var GET_RESOURCE = 'c1b1bf41-a4e6-4562-a880-72d9165a604e';
 module.exports = function(source, resourceType, cb) {
   console.log("source: ", source);
   client.invokeOperation(GET_RESOURCE, source, null, null, function(err, result) {
-    cb(err, result);
+    cb(err, {
+        resource: result
+    });
   })
 }
