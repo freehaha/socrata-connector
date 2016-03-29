1fb5c413182e9350249c3f603821048179e465f2
John Sun <freehaha@gmail.com>
2016-03-29 12:45:12 +1100

    upgrade apibase-client

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ var client = new APIBase({
 
 var GET_RESOURCE = 'c1b1bf41-a4e6-4562-a880-72d9165a604e';
 module.exports = function(source, resourceType, cb) {
-  console.log("source: ", source);
   client.invokeOperation(GET_RESOURCE, source, null, null, function(err, result) {
-    cb(err, {
-        resource: result
-    });
-  })
-}
+    var data = null;
+    try {
+      data = JSON.parse(result.body);
+      cb(err, {
+        resource: data
+      });
+    } catch(e) {
+      cb(e);
+    }
+  });
+};
